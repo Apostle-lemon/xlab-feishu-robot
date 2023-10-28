3dfb91b5e08772a7b9836c99dc905691181191b1
Aniurm <534147085@qq.com>
2023-10-28 11:22:30 +0800

    fix: send message

--- internal/event_handler/receive_message/example_group.go
+++ internal/event_handler/receive_message/example_group.go
@@ -1,15 +1,13 @@
 package receiveMessage
 
 import (
-	"xlab-feishu-robot/internal/pkg"
-
 	_ "github.com/sirupsen/logrus"
 )
 
-func init(){
+func init() {
 	groupMessageRegister(groupHelpMenu, "help")
 }
 
-func groupHelpMenu(messageevent *MessageEvent){
-	pkg.Cli.MessageSend("chat_id",messageevent.Message.Chat_id,"text","this is a group test string")
+func groupHelpMenu(messageevent *MessageEvent) {
+	SendMessage(GroupChatId, messageevent.Message.Chat_id, Text, "this is a group test string")
 }
--- internal/event_handler/receive_message/example_p2p.go
+++ internal/event_handler/receive_message/example_p2p.go
@@ -1,15 +1,13 @@
 package receiveMessage
 
 import (
-	"xlab-feishu-robot/internal/pkg"
-
 	_ "github.com/sirupsen/logrus"
 )
 
-func init(){
+func init() {
 	p2pMessageRegister(p2pHelpMenu, "help")
 }
 
-func p2pHelpMenu(messageevent *MessageEvent){
-	pkg.Cli.MessageSend("open_id",messageevent.Sender.Sender_id.Open_id,"text","this is a P2P test string")
+func p2pHelpMenu(messageevent *MessageEvent) {
+	SendMessage(UserOpenId, messageevent.Sender.Sender_id.Open_id, Text, "this is a p2p test string")
 }
